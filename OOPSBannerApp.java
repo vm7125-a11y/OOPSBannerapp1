3c669e70f5e688468c87bcddf13e2ee487f2d455
vm7125-a11y <vm7125@srmist.edu.in>
2026-03-22 13:19:50 +0530

    feature/uc8

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,17 +1,46 @@
+import java.util.*;
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[][] banner = {
-            {" ***** ", "*     *", "*     *", "*     *", "*     *", "*     *", " ***** "},
-            {" ***** ", "*     *", "*     *", "*     *", "*     *", "*     *", " ***** "},
-            {" ***** ", "*     *", "*     *", " ***** ", "*      ", "*      ", "*      "},
-            {" ***** ", "*      ", "*      ", " ***** ", "      *", "      *", " ***** "}
-        };
+        Map<Character, String[]> map = new HashMap<>();
 
-        for (int i = 0; i < banner[0].length; i++) {
-            for (int j = 0; j < banner.length; j++) {
-                System.out.print(banner[j][i] + "  ");
+        map.put('O', new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        });
+
+        map.put('P', new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            " ***** ",
+            "*      ",
+            "*      ",
+            "*      "
+        });
+
+        map.put('S', new String[]{
+            " ***** ",
+            "*      ",
+            "*      ",
+            " ***** ",
+            "      *",
+            "      *",
+            " ***** "
+        });
+
+        String text = "OOPS";
+
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < text.length(); j++) {
+                System.out.print(map.get(text.charAt(j))[i] + "  ");
             }
             System.out.println();
         }
