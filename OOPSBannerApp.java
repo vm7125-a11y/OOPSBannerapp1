22a305e77b6f4f83b6098be1a013701bc073968a
vm7125-a11y <vm7125@srmist.edu.in>
2026-03-22 13:03:54 +0530

    feature/UC5

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -3,15 +3,13 @@ public class OOPSBannerApp {
     public static void main(String[] args) {
 
         String[] banner = {
-
-            " *****    *****    *****    *****  ",
-            "*     *  *     *  *     *  *       ",
-            "*     *  *     *  *     *  *       ",
-            "*     *  *     *  *****     *****  ",
-            "*     *  *     *  *               *",
-            "*     *  *     *  *               *",
-            " *****    *****    *         *****  "
-
+            String.join(" ", " ***** ", " ***** ", " ***** ", " ***** "),
+            String.join(" ", "*     *", "*     *", "*     *", "*      "),
+            String.join(" ", "*     *", "*     *", "*     *", "*      "),
+            String.join(" ", "*     *", "*     *", " ***** ", " ***** "),
+            String.join(" ", "*     *", "*     *", "*      ", "      *"),
+            String.join(" ", "*     *", "*     *", "*      ", "      *"),
+            String.join(" ", " ***** ", " ***** ", "*      ", " ***** ")
         };
 
         for (String line : banner) {
